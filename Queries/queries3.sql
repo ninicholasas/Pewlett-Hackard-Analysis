1f161b816dea756eaf8406653799ab03bedbdbed
ninicholasas <110373282+ninicholasas@users.noreply.github.com>
2023-01-13 21:35:47 -0800

    Updating sql File

--- Queries/queries3.sql
+++ Queries/queries3.sql
@@ -38,3 +38,34 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+SELECT * FROM retirement_info;
+
+SELECT * FROM dept_info;
+-- Sales Info
+SELECT ri.emp_no, ri.first_name, ri.last_name, di.dept_name
+-- INTO sales_info
+FROM retirement_info as ri
+INNER JOIN dept_info as di
+ON (ri.emp_no = di.emp_no)
+WHERE (di.dept_name = 'Sales');
+
+
+
+
+--Sales and Development
+SELECT ri.emp_no, ri.first_name, ri.last_name, di.dept_name
+-- INTO sales_development_info
+FROM retirement_info as ri
+INNER JOIN dept_info as di
+ON (ri.emp_no = di.emp_no)
+WHERE di.dept_name IN ('Sales', 'Development');
+
+-- Retirement_Titles
+SELECT e.emp_no, e.first_name, e.last_name, t.from_date, t.to_date
+--INTO retirement_titles
+FROM employees as e 
+INNER JOIN titles as t 
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
